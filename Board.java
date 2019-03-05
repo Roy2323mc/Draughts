06bd4cb21a4842842263d980830c8df574a20d16
Li <lich1@egb070000007.lancs.local>
2019-03-05 21:19:13 +0000

    Not use gridlayout

--- Board.java
+++ Board.java
@@ -1,8 +1,5 @@
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.*;
 
 public class Board{
     public static void main(String[] args){
@@ -11,17 +8,22 @@ public class Board{
 	    frame.setTitle("Draughts");
 	    frame.setSize(800, 800);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel panel = new JPanel(new GridLayout(8, 8));
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(8,8));
+        panel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         int type=0;
+        JButton[][] button = new JButton[8][8];
+        button[1][1] = new JButton("Hi");
+        panel.add(button[1][1]);
         
-        for (int i = 0; i < 8; i++) {
-            for (int o = 0; o < 8; o++) {
-                ImageIcon x = new ImageIcon("/home/lancs/lich1/hdrive/scc110/Lent Term final/pieces/empty2.png");
-                JButton button = new JButton(x);
-                panel.add(button);
+        /*for (int o = 7; o > -1; o--) {
+            for (int i = 0; i < 8; i++) {
+                button[i][o].setSize(100,100);
+                button[i][o] = new JButton("Hi");
+                panel.add(button[i][o]);
             }
-        }
-
+        }*/
+    frame.add(panel);
 
 
     }
--- Square.java
+++ Square.java
@@ -1,24 +1,11 @@
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.Dimension;
+import java.awt.*;
+
 
 public class Square{
-    public void createSquare(int type, int x, int y){
+    public void Square(int type, int x, int y){
 
-        switch(type){
-            case 0: {
-            ImageIcon i = new ImageIcon("/home/lancs/lich1/hdrive/scc110/Lent Term final/pieces/empty.png");
-            JButton button = new JButton(i);
-            break;     
-            }
-            case 1: {
-            ImageIcon i = new ImageIcon("/home/lancs/lich1/hdrive/scc110/Lent Term final/pieces/empty2.png");
-            JButton button = new JButton(i);
-            break;  
-            }
-
-        }
+        
 
 
     }
